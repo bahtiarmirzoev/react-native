1a79e5cf1092a6ba058d99fdc20701019c90d499
Rufat Aliyev <rufat845@gmail.com>
2025-06-13 21:40:36 +0400

    Add initial iOS project files and configurations
    - Created Info.plist with essential app metadata and configurations.
    - Added PrivacyInfo.xcprivacy to specify accessed APIs and privacy tracking settings.
    - Implemented SplashScreen.storyboard for the app's launch screen design.
    - Established Expo.plist for update configurations.
    - Added bridging header for Swift interoperability.
    - Created empty entitlements file for app capabilities.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,29 +1,40 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-import 'react-native-reanimated';
-
-import { useColorScheme } from '@/hooks/useColorScheme';
-
+import { useFonts } from "expo-font";
+import { router, SplashScreen, Stack } from "expo-router";
+import { useEffect } from "react";
+SplashScreen.preventAutoHideAsync();
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
-  const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+  const [loaded, error] = useFonts({
+    "Lexend-Thin": require("../assets/fonts/Lexend-Thin.ttf"),
+    "Lexend-ExtraLight": require("../assets/fonts/Lexend-ExtraLight.ttf"),
+    "Lexend-Light": require("../assets/fonts/Lexend-Light.ttf"),
+    "Lexend-Regular": require("../assets/fonts/Lexend-Regular.ttf"),
+    "Lexend-Medium": require("../assets/fonts/Lexend-Medium.ttf"),
+    "Lexend-SemiBold": require("../assets/fonts/Lexend-SemiBold.ttf"),
+    "Lexend-Bold": require("../assets/fonts/Lexend-Bold.ttf"),
+    "Lexend-ExtraBold": require("../assets/fonts/Lexend-ExtraBold.ttf"),
+    "Lexend-Black": require("../assets/fonts/Lexend-Black.ttf"),
   });
 
-  if (!loaded) {
-    // Async font loading only occurs in development.
+  useEffect(() => {
+    if (loaded || error) {
+      SplashScreen.hideAsync();
+
+      router.push("/(intro)");
+    }
+  }, [loaded, error]);
+
+  if (!loaded && !error) {
     return null;
   }
-
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
+    <Stack
+      screenOptions={{
+        headerShown: false,
+      }}
+    >
+      <Stack.Screen name="(intro)" />
+      <Stack.Screen name="(tabs)" />
+      <Stack.Screen name="(auth)" />
+    </Stack>
   );
 }

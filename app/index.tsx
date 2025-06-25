d3203f8a652f72e1c743abfcf6071792741e41a6
Mac <mac@Bakhtiyars-MacBook-Air.local>
2025-06-25 16:30:41 -0700

    added fonts

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,32 @@
+import { useFonts } from 'expo-font';
+import { SplashScreen } from "expo-router";
+import { useEffect } from "react";
 import { Text, View } from "react-native";
 
+SplashScreen.preventAutoHideAsync();
 export default function Index() {
+   const [loaded, error] = useFonts({
+    'Jost-Thin': require('../assets/fonts/Jost-Thin.ttf'),
+    'Jost-Light': require('../assets/fonts/Jost-Light.ttf'),
+    'Jost-Regular': require('../assets/fonts/Jost-Regular.ttf'),
+    'Jost-Medium': require('../assets/fonts/Jost-Medium.ttf'),
+    'Jost-SemiBold': require('../assets/fonts/Jost-SemiBold.ttf'),
+    'Jost-Bold': require('../assets/fonts/Jost-Bold.ttf'),
+    'Jost-Black': require('../assets/fonts/Jost-Black.ttf'),
+    'Jost-ExtraBold': require('../assets/fonts/Jost-ExtraBold.ttf'),
+    
+  });
+
+  useEffect(() => {
+    
+    if (loaded || error) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded, error]);
+
+  if (!loaded && !error) {
+    return null;
+  }
   return (
     <View
       style={{
@@ -13,3 +39,4 @@ export default function Index() {
     </View>
   );
 }
+
